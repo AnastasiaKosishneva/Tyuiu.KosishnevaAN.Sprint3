058236d9ac2bb1f8716befe5bc7ff8bf9c0933ed
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-10-21 23:15:46 +0500

    Task2

---
+++ Tyuiu.KosishnevaAN.Sprint3.Task2.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosishnevaAN.Sprint3.Task2.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosishnevaAN.Sprint3.Task2.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2b3b2fa-b0e3-4106-8a98-8409d0c0eda5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KosishnevaAN.Sprint3.Task2.V5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KosishnevaAN.Sprint3.Task2.V5.Lib;
+
+namespace Tyuiu.KosishnevaAN.Sprint3.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Косишнева А. Н. | ИСТНб-23-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #3                                                             *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                         *");
+            Console.WriteLine("* Задание #2                                                            *");
+            Console.WriteLine("* Вариант #5                                                            *");
+            Console.WriteLine("* Выполнила: Косишнева Анастасия Николаенвна | ИСТНб-23-1               *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет            *");
+            Console.WriteLine("* произведение ряда по формуле, при a=0,75                              *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            int startValue = 1;
+            int stopValue = 20;
+            double value = 0.75;
+            Console.WriteLine("Переменаая A = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("Сумма рада = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+    
+}
